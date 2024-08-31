a2662a492c4b380020c5e4228594d272d78413dc
George Leyva <leyvageorge@hotmail.com>
2024-08-31 14:03:08 -0700

    changes to pipe variables

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -2,15 +2,17 @@ extends Node2D
 
 class_name Pipes
 
+const OFF_SCREEN: float = -500.0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x -= delta * 120.0
+	position.x -= delta * GameManager.SCROLL_SPEED
 	check_off_screen() #remove when it's not being rendered 
 
 
 func check_off_screen() -> void:
-	if position.x < -500.0:
+	if position.x < OFF_SCREEN:
 		queue_free()
 
 
