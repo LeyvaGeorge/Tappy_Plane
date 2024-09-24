514104b3a2bccb7b7baf779884d1ef35bffcbc38
George Leyva <leyvageorge@hotmail.com>
2024-09-24 09:15:10 -0700

    Finished Game

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -26,6 +26,7 @@ func _on_screen_exited() -> void:
 
 
 func _on_pipe_body_entered(body: Node2D) -> void:
+	print("Pipe ", body)
 	if body is Tappy:
 		body.die()
 	#if body.is_in_group(GameManager.GROUP_PLANE) == true:
